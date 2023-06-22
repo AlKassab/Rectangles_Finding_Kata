9e9c1c30f49871c95502e5c6c8397224875333bd
inter <inter@PCOPSTIM>
2023-06-22 13:49:19 +0000

    Ajoutez des fichiers projet.

---
+++ BitmapExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Kata
+{
+    internal static class BitmapExtensions
+    {
+        private static double CompareTo(this Color e1, Color e2)
+        {
+            long rmean = (e1.R + e2.R) / 2;
+            long r = e1.R - e2.R;
+            long g = e1.G - e2.G;
+            long b = e1.B - e2.B;
+            var diff = Math.Sqrt((((512 + rmean) * r * r) >> 8) + 4 * g * g + (((767 - rmean) * b * b) >> 8));
+
+            return diff;
+        }
+        private static bool IsContrast(this Color pixel) => pixel.CompareTo(Color.White) > 50;
+
+        public static Rectangle[] ExtractRectangles(this Bitmap img)
+        {
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            bool[,] visited = new bool[img.Width, img.Height];
+
+            // Parcours de l'image pixel par pixel
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    if (!visited[x, y])
+                    {
+                        Color pixel = img.GetPixel(x, y);
+
+                        // Vérifier si le pixel est contrasté
+                        if (pixel.IsContrast())
+                        {
+                            Rectangle rectangle = FindRectangle(img, x, y, visited);
+                            rectangles.Add(rectangle);
+                        }
+                    }
+                }
+            }
+
+            return rectangles.ToArray();
+        }
+
+        private static Rectangle FindRectangle(Bitmap img, int startX, int startY, bool[,] visited)
+        {
+            int endX = startX;
+            int endY = startY;
+
+            // Trouver la limite la plus à droite du rectangle
+            while (endX < img.Width && !visited[endX, startY] && img.GetPixel(endX, startY).IsContrast())
+            {
+                visited[endX, startY] = true;
+                endX++;
+            }
+
+            // Trouver la limite la plus à droite du rectangle
+            for (int x = startX; x < endX; x++)
+            {
+                for (int y = startY + 1; y < img.Height; y++)
+                {
+                    if (!visited[x, y] && img.GetPixel(x, y).IsContrast())
+                    {
+                        visited[x, y] = true;
+                        endY = Math.Max(endY, y);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return new Rectangle(startX, startY, endX - startX, endY - startY);
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Kata
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var img = new Bitmap(Image.FromFile(@"C:\Users\inter\Downloads\Kata_Squares\2.jpg"));
+            var resultImg = new Bitmap(img.Width, img.Height);
+
+            var rectangles = img.ExtractRectangles();
+
+            foreach (var rec in rectangles)
+            {
+                Console.WriteLine(rec);
+            }
+
+            using (Graphics g = Graphics.FromImage(resultImg))
+            {
+                // Ajouter un Background Blanc à l'image
+                g.Clear(Color.White);
+
+                // Dessiner les rectangles sur l'image
+                foreach (Rectangle rectangle in rectangles)
+                {
+                    g.DrawRectangle(Pens.Black, rectangle);
+                }
+
+                // Enregistrer l'image avec les rectangles dessinés
+                resultImg.Save(@"C:\Users\inter\Downloads\Kata_Squares\image_with_rectangles_result.png", ImageFormat.Png);
+            }
+        }
+    }
+}
